b73ab96fe69fe5084b1ad520282ec19967899a55
Michael5Kolesnikov <86573517+Michael5Kolesnikov@users.noreply.github.com>
2022-08-08 12:41:13 +0300

    Update main.py
    Added headless, added iteration completion time output to display work
    Добавлен headless, добавлен вывод времени завершения итерации для отображения работы

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import webbrowser
+from selenium.webdriver.chrome.options import Options
+from webbrowser import open
 from os import startfile
 from time import sleep
 from datetime import datetime
@@ -10,15 +11,17 @@ path = "https://grandtrain.ru/tickets/2078001-2000000"  # Симф
 i = 0
 desired = "Осталось мест"
 while True:
-    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
     driver.get(f"{path}/2{m[i]}.08.2022/")
     sleep(2)
     text = driver.page_source
     driver.close()
     driver.quit()
     if desired in text:
-        print(f"2{m[i]}")
-        webbrowser.open(f"{path}/2{m[i]}.08.2022/")
+        print(f"2{m[i]} -- покупай!!!")
+        open(f"{path}/2{m[i]}.08.2022/")
         startfile("Rockstar.mp3")
         break
     i += 1
