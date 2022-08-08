d13a3da05f5c97f36cad075bb95159cf5ad2f28f
Michael5Kolesnikov <86573517+Michael5Kolesnikov@users.noreply.github.com>
2022-08-08 14:25:34 +0300

    Update main.py
    Added notification to VK
    Добавлено оповещение в ВК

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from webbrowser import open
 from os import startfile
 from time import sleep
 from datetime import datetime
+from VK_bot import send_message, id1, id2
 
 m = [2, 3, 4]
 path = "https://grandtrain.ru/tickets/2078001-2000000"  # Симф
@@ -11,17 +12,20 @@ path = "https://grandtrain.ru/tickets/2078001-2000000"  # Симф
 i = 0
 desired = "Осталось мест"
 while True:
+    url = f"{path}/2{m[i]}.08.2022/"
     options = Options()
     options.headless = True
     driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
-    driver.get(f"{path}/2{m[i]}.08.2022/")
+    driver.get(url)
     sleep(2)
     text = driver.page_source
     driver.close()
     driver.quit()
     if desired in text:
+        send_message(id1, url)
+        send_message(id2, url)
         print(f"2{m[i]} -- покупай!!!")
-        open(f"{path}/2{m[i]}.08.2022/")
+        open(url)
         startfile("Rockstar.mp3")
         break
     i += 1
